9d57e76254f4aa0129771ae6f55f17403c9ae47c
J Maine <33839097+xjmaine@users.noreply.github.com>
2021-08-25 21:05:56 +0000

    Add files via upload

---
+++ Part 1/Example3.java
@@ -0,0 +1,25 @@
+/**
+ * This is a self-test based on 
+ * what is covered in part one 
+ */
+
+/**
+ * As we saw in the examples, we used the ‘/’ to work out the division of var1
+ * by var2. Given that ‘+’ would perform addition, ‘-‘ subtraction and ‘*’
+ * multiplication, write out a program which performs all thenamed operations by
+ * using two integer values which are hard coded into the program. Hints:  You
+ * need only two variables of type integer  Make one variable larger and
+ * divisible by the other  You can perform the required calculations directly
+ * in the print statements, remember to enclose the operation within brackets,
+ * e.g. (var1-var2)
+ */
+
+public class Example3 {
+
+    public static void main(String[] args) {
+        // declare and initialize variables
+
+        System.out.println();
+    }
+
+}
