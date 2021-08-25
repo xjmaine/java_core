ae5591d67db2d74ebede5f355c1dfba46d0829c6
J Maine <33839097+xjmaine@users.noreply.github.com>
2021-08-25 20:59:05 +0000

    Add files via upload

---
+++ Part 1/Example2.java
@@ -0,0 +1,23 @@
+/**
+ * working with variables and data types
+ */
+
+// this is a single line comment
+
+public class Example2 {
+
+    // public, private, protected, package
+
+    public static void main(String[] args) {
+        // variable : containers for values
+        int var1; // this declares a variable
+        int var2; // this declares a another variable
+
+        var1 = 150;
+        var2 = 5;
+        int var3 = var1 / var2;
+
+        // 150/5 = ???
+        System.out.println(var3);
+    }
+}
---
+++ Part 1/HelloWorld.java
@@ -0,0 +1,11 @@
+
+/**
+ * This is out first java program
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
